53e1d1f070effb969bded81202e51dd9d1f0de20
Hovhannes Sahakyan <hovhannes@superannotate.com>
2024-07-08 11:41:25 +0400

    Init development environment

---
+++ webpack.config.mjs
@@ -0,0 +1,26 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: path.resolve(__dirname, 'src/main.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html' })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
+}
